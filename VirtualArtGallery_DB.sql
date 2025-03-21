4ed51e2aba71c3e6338795f7af054301abd6dcd1
dhayanandhs2003 <sakthideepa77@gmail.com>
2025-03-21 17:21:18 +0530

    created all the tables in VisualArtGallery

--- VirtualArtGallery_DB.sql
+++ VirtualArtGallery_DB.sql
@@ -29,3 +29,50 @@ create table artist (
     contact_info varchar(255)
 );
 
+-- creating user table
+-- i have not added the favorite_artworks column inside the user table because of 1NF Each column should contain atomic (indivisible) values
+-- and it is difficult to write qurries so i have made a junction table
+
+create table user (
+    user_id int primary key auto_increment,
+    username varchar(50) not null unique,
+    password varchar(255) not null,
+    email varchar(255) not null unique,
+    first_name varchar(100),
+    last_name varchar(100),
+    date_of_birth date,
+    profile_picture varchar(255)
+);
+
+-- creating gallery table
+
+create table gallery (
+    gallery_id int primary key auto_increment,
+    name varchar(255) not null,
+    description text,
+    location varchar(255),
+    curator_id int, -- reference for artist id
+    opening_hours varchar(255),
+    foreign key (curator_id) references artist(artist_id) on delete set null
+);
+
+-- creating favorite_artwork table mentioned in the user favorite artwork part many to many
+
+create table favorite_artwork (
+    user_id int,
+    artwork_id int,
+    primary key (user_id, artwork_id),
+    foreign key (user_id) references user(user_id) on delete cascade,
+    foreign key (artwork_id) references artwork(artwork_id) on delete cascade
+);
+
+-- creating artwork_gallery table mentioned in the many to many section
+
+create table artwork_gallery (
+    artwork_id int,
+    gallery_id int,
+    primary key (artwork_id, gallery_id),
+    foreign key (artwork_id) references artwork(artwork_id) on delete cascade,
+    foreign key (gallery_id) references gallery(gallery_id) on delete cascade
+);
+
